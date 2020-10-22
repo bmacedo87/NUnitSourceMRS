f468157777693c1fa7f93ff7c966ee032caab69c
Damart <limbo12_@hotmail.com>
2020-10-22 14:39:46 -0500

    SentRequest Feature

--- MarsQA-1/NunitTests/ManageRequestTest/ManageReceivedRequestTest.cs
+++ MarsQA-1/NunitTests/ManageRequestTest/ManageReceivedRequestTest.cs
@@ -3,15 +3,15 @@ using MarsQA_1.Pages;
 using MarsQA_1.Utils;
 using NUnit.Framework;
 
-namespace MarsQA_1.NunitTests
+namespace MarsQA_1.NunitTests.ManageRequest
 {
     [TestFixture]
-    class ManageRequestTest:Start
+    class ManageReceivedRequestTest:Start
     {
         [Test][Category("TC-013-02")]
         public static void ValidateReceivedRequest()
         {
-            ManageRequestPage.GenerateNewRequest();
+            ManageRequestPage.GenerateNewRequest(5);
             SignIn.OpenForm();
             SignIn.FillCredentials(2);
             ManageRequestPage.NavigateToReceivedRequest();
@@ -22,7 +22,7 @@ namespace MarsQA_1.NunitTests
         [Category("TC-013-07")]
         public static void ValidateAcceptRequest()
         {
-            ManageRequestPage.GenerateNewRequest();
+            ManageRequestPage.GenerateNewRequest(5);
             SignIn.OpenForm();
             SignIn.FillCredentials(2);
             ManageRequestPage.NavigateToReceivedRequest();
@@ -34,7 +34,7 @@ namespace MarsQA_1.NunitTests
         [Category("TC-013-08")]
         public static void ValidateDeclineRequest()
         {
-            ManageRequestPage.GenerateNewRequest();
+            ManageRequestPage.GenerateNewRequest(5);
             SignIn.OpenForm();
             SignIn.FillCredentials(2);
             ManageRequestPage.NavigateToReceivedRequest();
